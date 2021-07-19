47529c43c1f040bc60c0ba76e3a6a5f9e26d55cd
YudexiOrozco <86724658+YudexiOrozco@users.noreply.github.com>
2021-07-19 18:51:18 -0500

    Empezando estructura en html y data.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,13 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+//El corazon de este proyecto es la manipulación da datos a través de arreglos y objetos
+//Aqui toda la funcionalidad que corresponda a obtener, procesar y manipular datos (tus funciones)
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+// import { example } from './data.js';
+
+// import data from './data/ghibli/ghibli.js';
+
+// import data from './data/pokemon/pokemon.js';
+
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// console.log(example, data);
+
+
+
+function getData() {
+  fetch("data/ghibli/ghibli.json")
+  .then((res) => res.json())
+  .then((data) => {
+    getFilm(data.films);
+  });
+}
+
+
+function getFilm(films) {
+  for (let i = 0; i < films.length; i++) {
+    console.log(films[i].release_date);
+  }
+}
+
+getData();
+
